51296d0c8dde6362fd88271fa65d2431b9f22d48
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-06-14 01:42:58 -0700

    feat: add mixpanel

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Router from './Router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from '@/components/theme-provider'
+import mixpanel from "mixpanel-browser"
+
+mixpanel.init(import.meta.env.VITE_MP_TOKEN, {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
 
 const queryClient = new QueryClient()
 
@@ -12,7 +19,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <App />
+        <Router />
         <Toaster />
       </ThemeProvider>
     </QueryClientProvider>
